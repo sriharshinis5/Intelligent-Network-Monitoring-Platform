09e9251119932daf90df15c0f059791e30d4a04f
sri harshini s <sit23ec003@sairamtap.edu.in>
2026-06-12 13:41:58 +0530

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Network Monitoring Dashboard")
+
+data = pd.read_csv("data/network_logs.csv")
+
+st.dataframe(data.head())
+
+st.line_chart(data["Latency"])
+
+st.line_chart(data["Throughput"])
+
+st.bar_chart(data["Label"].value_counts())
