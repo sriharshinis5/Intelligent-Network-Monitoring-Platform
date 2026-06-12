366d3f81d0726568b149864fcf4340b71216b1bd
sri harshini s <sit23ec003@sairamtap.edu.in>
2026-06-12 13:45:59 +0530

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+data = pd.read_csv("data/network_logs.csv")
+
+plt.figure(figsize=(8,5))
+plt.plot(data["Latency"])
+plt.title("Network Latency Analysis")
+plt.xlabel("Sample")
+plt.ylabel("Latency (ms)")
+plt.grid()
+
+plt.savefig("results/traffic_analysis.png")
+
+print("Graph saved.")
