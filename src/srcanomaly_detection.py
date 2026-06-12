6d549b6a568b2f8781241e37d29e99e1fa817ffe
sri harshini s <sit23ec003@sairamtap.edu.in>
2026-06-12 13:41:28 +0530

    Update srcanomaly_detection.py

--- src/srcanomaly_detection.py
+++ src/srcanomaly_detection.py
@@ -1,1 +1,31 @@
+import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+data = pd.read_csv("data/network_logs.csv")
+
+X = data[[
+    "Latency",
+    "PacketLoss",
+    "Throughput",
+    "BandwidthUsage"
+]]
+
+y = data["Label"]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train,y_train)
+
+pred = model.predict(X_test)
+
+print(classification_report(y_test,pred))
